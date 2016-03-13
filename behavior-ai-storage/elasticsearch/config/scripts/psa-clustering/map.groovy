eb7bdf8ec2e07d2f0fb006032cd88c7633f3b9e4
Chris <dev@krzysztofsobczak.com>
2016-03-13 16:00:11 +0100

    include timestamp

--- behavior-ai-storage/elasticsearch/config/scripts/psa-clustering/map.groovy
+++ behavior-ai-storage/elasticsearch/config/scripts/psa-clustering/map.groovy
@@ -1,4 +1,8 @@
-userVisit = [id: doc['clientip.raw'].value + doc['agent.raw'].value, path: [doc['path_category.raw'].value]];
+userVisit = [
+        id: doc['clientip.raw'].value + doc['agent.raw'].value,
+        timestamp: doc['timestamp'].value,
+        path: [doc['path_category.raw'].value]
+];
 for (user in _agg.users) {
     if (user.id == userVisit.id) {
         user.path.add(userVisit.path[0]); return;
